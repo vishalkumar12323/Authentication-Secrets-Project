2472e71da9b8119e3f29176a9587b4aeeb06ac36
vishal <vishalninaniya8949info@gmail.com>
2023-12-03 15:04:37 +0530

    Level 2 authentication security encryption, hashing-password done

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ dotenv.config();
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import encrypt from "mongoose-encryption";
+import bcrypt from "bcrypt";
+// import encrypt from "mongoose-encryption";
+// import md5 from "md5";
 
 const app = express();
 const port = 3000;
@@ -14,10 +16,10 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-userSchema.plugin(encrypt, {
-  secret: process.env.ENCRYPTION_KEY,
-  encryptedFields: ["password"],
-});
+// userSchema.plugin(encrypt, {
+//   secret: process.env.ENCRYPTION_KEY,
+//   encryptedFields: ["password"],
+// });
 const User = new mongoose.model("user", userSchema);
 
 app.use(express.static("public"));
@@ -43,18 +45,21 @@ app.post("/register", async function (req, res) {
   const username = req.body.username;
   const password = req.body.password;
   try {
-    const newUser = new User({
-      username: username,
-      password: password,
+    bcrypt.hash(password, 10, function (err, hash) {
+      console.log(hash);
+      const newUser = new User({
+        username: username,
+        password: hash,
+      });
+      if (!newUser) {
+        res
+          .status(400)
+          .send("<h3> Fill the email and password feild currectly </h3>");
+      } else {
+        newUser.save();
+        res.render("secrets.ejs");
+      }
     });
-    if (!newUser) {
-      res
-        .status(400)
-        .send("<h3> Fill the email and password feild currectly </h3>");
-    } else {
-      newUser.save();
-      res.render("secrets.ejs");
-    }
   } catch (error) {
     console.log(error);
   }
@@ -65,11 +70,15 @@ app.post("/login", async (req, res) => {
   const password = req.body.password;
   try {
     const foundUser = await User.findOne({ username: username });
-    if (foundUser.username === username && foundUser.password === password) {
-      res.render("secrets.ejs");
-    } else {
-      res.status(400).send("<h3> Invalid Credentials </h3>");
-    }
+    bcrypt.compare(password, foundUser.password, function (err, result) {
+      console.log(result);
+      if (result == true) {
+        res.render("secrets.ejs");
+      } else {
+        console.log(err);
+        res.status(400).send("<h3> Invalid Credentials </h3>");
+      }
+    });
   } catch (error) {
     console.log(error);
   }
