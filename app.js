8ec7f2402a85537de753fb73a052c01f5f27cc83
vishal <vishalninaniya8949info@gmail.com>
2023-12-04 14:15:14 +0530

    Level 3 authentication added using passport js

--- app.js
+++ app.js
@@ -3,12 +3,33 @@ dotenv.config();
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
+import session from "express-session";
+import passport from "passport";
+import passportLocalMongoose from "passport-local-mongoose";
+// import bcrypt from "bcrypt";
 // import encrypt from "mongoose-encryption";
 // import md5 from "md5";
 
 const app = express();
 const port = 3000;
+
+app.use(express.static("public"));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(
+  session({
+    secret: "our little secret.",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect(process.env.DATABASE_URL);
 const userSchema = new mongoose.Schema({
@@ -20,14 +41,13 @@ const userSchema = new mongoose.Schema({
 //   secret: process.env.ENCRYPTION_KEY,
 //   encryptedFields: ["password"],
 // });
+userSchema.plugin(passportLocalMongoose);
 const User = new mongoose.model("user", userSchema);
 
-app.use(express.static("public"));
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
@@ -41,47 +61,56 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async function (req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
-  try {
-    bcrypt.hash(password, 10, function (err, hash) {
-      console.log(hash);
-      const newUser = new User({
-        username: username,
-        password: hash,
-      });
-      if (!newUser) {
-        res
-          .status(400)
-          .send("<h3> Fill the email and password feild currectly </h3>");
-      } else {
-        newUser.save();
-        res.render("secrets.ejs");
-      }
-    });
-  } catch (error) {
-    console.log(error);
+app.get("/secrets", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("secrets.ejs");
+  } else {
+    res.redirect("/login");
   }
 });
 
-app.post("/login", async (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  try {
-    const foundUser = await User.findOne({ username: username });
-    bcrypt.compare(password, foundUser.password, function (err, result) {
-      console.log(result);
-      if (result == true) {
-        res.render("secrets.ejs");
-      } else {
+app.get("/logout", (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/login");
+    }
+  });
+});
+app.post("/register", function (req, res) {
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    function (err, user) {
+      if (err) {
         console.log(err);
-        res.status(400).send("<h3> Invalid Credentials </h3>");
+        res.redirect("/register");
+      } else {
+        console.log(user);
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/secrets");
+        });
       }
-    });
-  } catch (error) {
-    console.log(error);
-  }
+    }
+  );
+});
+
+app.post("/login", async (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  req.login(user, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/secrets");
+      });
+    }
+  });
 });
 app.listen(port, () => {
   console.log(`server listening on url http://localhost:${port}`);
